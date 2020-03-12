ea198d4a7b6cb32dcedf92016b8fa6b149b1cf8a
André Lucas SIlva Verdelho <62015174+LucasVerdelho@users.noreply.github.com>
2020-03-12 17:30:21 +0000

    Update dados.c

--- dados.c
+++ dados.c
@@ -8,7 +8,7 @@ ESTADO *inicializar_estado(){
     COORDENADA j1 = {3,4};
     COORDENADA j2 = {4,4};
     JOGADA jogada0 = {j1,j2};
-    e->jogadas[0] = jogada1;
+    e->jogadas[0] = jogada0;
     for (int linha = 0; linha < 8; linha++)
     {
         for (int coluna = 0; coluna < 8; coluna++)
