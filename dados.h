a8860601d08e28026714e164e8efb1bd7ca453fb
João Tiago Alves Fidalgo de Sousa <62015233+tsousa111@users.noreply.github.com>
2020-03-12 19:36:02 +0000

    Update dados.h

--- dados.h
+++ dados.h
@@ -3,22 +3,19 @@
 
 typedef enum {VAZIO, BRANCA, PRETA} CASA;
 
-typedef struct 
-{
-    int coluna;
+typedef struct {
     int linha;
+    int coluna;
 } COORDENADA;
 
-typedef struct 
-{
+typedef struct {
     COORDENADA jogador1;
     COORDENADA jogador2;
 } JOGADA;
 
 typedef JOGADA JOGADAS[32];
 
-typedef struct 
-{
+typedef struct {
     CASA tab[8][8];
     COORDENADA ultima_jogada;
     JOGADAS jogadas;
