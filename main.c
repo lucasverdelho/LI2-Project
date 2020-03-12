baeb720a6a0eb4ec2a35309748122c02b761b36c
João Tiago Alves Fidalgo de Sousa <62015233+tsousa111@users.noreply.github.com>
2020-03-12 16:43:41 +0000

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,19 +9,3 @@ int main(){
     interpretador(e);
     return 0;
 }
-
-=======
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "dados.h"
-
-// Função que deve ser colocada no ficheiro main.c
-int main()
-{
-ESTADO *e = inicializar_estado();
-interpretador(e);
-return 0;
-}
-
->>>>>>> 820c42990d7fbb2579676b811ca3c3c7c803f0f3
