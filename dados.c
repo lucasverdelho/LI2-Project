0fd672d685a1d35e4dcd05346813e1e3eb0dae86
João Tiago Alves Fidalgo de Sousa <62015233+tsousa111@users.noreply.github.com>
2020-03-12 16:41:13 +0000

    Update dados.c

--- dados.c
+++ dados.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include "dados.h"
 
@@ -27,21 +26,3 @@ int obter_numero_de_jogadas(ESTADO *estado){
 int obter_estado_casa(ESTADO *e, COORDENADA c){
 
 }
-=======
-#include <stdio.h>
-#include "dados.h"
-
-ESTADO *inicializar_estado(){
-    ESTADO *e = (ESTADO *) malloc(sizeof(ESTADO));
-    e->jogador_atual = 1;
-    e->num_jogadas = 0;
-    e->jogadas = {{5,5},{5,5}};
-    e->ultima_jogada = {0,0};
-    e->tab[5][5] = '*';
-    return e;
-}
-
-int obter_jogador_atual(ESTADO *estado){}
-int obter_numero_de_jogadas(ESTADO *estado){}
-int obter_estado_casa(ESTADO *e, COORDENADA c){}
->>>>>>> 820c42990d7fbb2579676b811ca3c3c7c803f0f3
