c9646429159dd1ff3f30d5fc078d2e5019cc64df
André Lucas SIlva Verdelho <62015174+LucasVerdelho@users.noreply.github.com>
2020-03-12 17:29:01 +0000

    Update dados.c

--- dados.c
+++ dados.c
@@ -5,9 +5,19 @@ ESTADO *inicializar_estado(){
     ESTADO *e = (ESTADO *) malloc(sizeof(ESTADO));
     e->jogador_atual = 1;
     e->num_jogadas = 0;
-    e->jogadas = {};
-    e->ultima_jogada = {0,0};
-    e->tab[4][4] = BRANCA;
+    COORDENADA j1 = {3,4};
+    COORDENADA j2 = {4,4};
+    JOGADA jogada1 = {j1,j2};
+    e->jogadas[0] = jogada1;
+    for (int linha = 0; linha < 8; linha++)
+    {
+        for (int coluna = 0; coluna < 8; coluna++)
+        {
+            if (linha == 3 && coluna == 4) 
+                e->tab[coluna][linha] = BRANCA;
+            e->tab[coluna][linha] = VAZIO;
+        }
+    }
     return e;
 }
 
