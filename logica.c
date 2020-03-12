e9d6a803d383b8c379529ecacba8526dafebba88
André Lucas SIlva Verdelho <62015174+LucasVerdelho@users.noreply.github.com>
2020-03-12 16:43:45 +0000

    Update logica.c

--- logica.c
+++ logica.c
@@ -6,13 +6,4 @@ int jogar(ESTADO *e, COORDENADA c) {
     printf("jogar %d %d\n", c.coluna, c.linha);
     return 1;
 }
-=======
-#include <stdio.h>
-#include "dados.h"
-
-// Função que deve ser completada e colocada na camada da lógica do programa
-int jogar(ESTADO *e, COORDENADA c) {
-    printf("jogar %d %d\n", c.coluna, c.linha);
-    return 1;
-}
 
