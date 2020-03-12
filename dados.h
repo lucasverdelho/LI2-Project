95fdc321114aca42c4ddddeb270175b0202d532c
João Tiago Alves Fidalgo de Sousa <62015233+tsousa111@users.noreply.github.com>
2020-03-12 17:41:14 +0000

    Update dados.h

--- dados.h
+++ dados.h
@@ -32,7 +32,6 @@ int obter_jogador_atual(ESTADO *estado);
 
 int obter_numero_de_jogadas(ESTADO *estado);
 
-int obter_estado_casa(ESTADO *e, COORDENADA c);
+CASA obter_estado_casa(ESTADO *e, COORDENADA c);
 
 #endif
-
