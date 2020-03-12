5331642b20bf54608e81a141a96c0d905c2e0b0c
João Tiago Alves Fidalgo de Sousa <62015233+tsousa111@users.noreply.github.com>
2020-03-12 19:36:39 +0000

    Update interface.c

--- interface.c
+++ interface.c
@@ -34,7 +34,7 @@ int interpretador(ESTADO *e) {
     if(fgets(linha, BUF_SIZE, stdin) == NULL)
         return 0;
     if(strlen(linha) == 3 && sscanf(linha, "%[a-h]%[1-8]", col, lin) == 2) {
-        COORDENADA coord = {*col - 'a', *lin - '1'};
+        COORDENADA coord = {*lin - '1', *col - 'a'};
         jogar(e, coord);
         mostrar_tabuleiro(e);
     }
