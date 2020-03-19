c71e7923f3065e980e757a38dc11442d862573aa
João Tiago Alves Fidalgo de Sousa <62015233+tsousa111@users.noreply.github.com>
2020-03-19 18:40:26 +0000

    Update interface.c

--- interface.c
+++ interface.c
@@ -5,12 +5,10 @@
 
 // Função que deve ser completada e colocada na camada de interface
 void mostrar_tabuleiro(ESTADO *e) {
-    COORDENADA c;
     for(int linha = 0; linha < 8; linha++){
         printf("%d ",8-linha);
         for(int coluna = 0; coluna < 8; coluna++){
-            c.linha = linha;
-            c.coluna = coluna;
+            COORDENADA c = {linha, coluna};
             switch(obter_estado_casa(e,c)){
                 case UM : 
                     putchar('1');
@@ -34,16 +32,32 @@ void mostrar_tabuleiro(ESTADO *e) {
     printf("  abcdefgh\n");
 }
 
+void print_erro(ERROS e){
+    char *lista_erros[] = {
+        "OK",
+        "Coordenada invalida",
+        "Jogada invalida",
+        "Erro de leitura do tabuleiro",
+        "Erro ao abrir o ficheiro"
+    };
+    if(e != OK)
+        printf("%s\n",lista_erros[e]);
+}
+
 // Função que deve ser completada e colocada na camada de interface
 int interpretador(ESTADO *e) {
     char linha[BUF_SIZE];
     char col[2], lin[2];
+    e->num_comando++;
+    printf("# %02d Player%d (%d)",e->num_comando,e->jogador_atual,e->num_jogadas);
     if(fgets(linha, BUF_SIZE, stdin) == NULL)
         return 0;
     if(strlen(linha) == 3 && sscanf(linha, "%[a-h]%[1-8]", col, lin) == 2) {
         COORDENADA coord = {*lin - '1', *col - 'a'};
-        jogar(e, coord);
-        mostrar_tabuleiro(e);
+        ERROS validar;
+        if(validar = jogar(e, coord) == OK)
+            mostrar_tabuleiro(e);
     }
+    else print_erro()
     return 1;
 }
