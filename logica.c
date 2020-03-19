d3c28e35c9ed8355c395c924d0aaf55c968913e2
João Tiago Alves Fidalgo de Sousa <62015233+tsousa111@users.noreply.github.com>
2020-03-19 16:32:32 +0000

    Update logica.c

--- logica.c
+++ logica.c
@@ -3,10 +3,10 @@
 
 // Função que deve ser completada e colocada na camada da lógica do programa
 
-// modificar o estado ao jogar na casa correta se a jogada for válida.
+// Modificar o estado ao jogar na casa correta se a jogada for válida.
 
 int jogar(ESTADO *e, COORDENADA c) {
-    printf("jogar %d %d\n", c.coluna, c.linha);
+    printf("jogar %d %d\n", c.linha, c.coluna);
     e->tab[c.linha][c.coluna] = BRANCA;
     e->tab[e->ultima_jogada.linha][e->ultima_jogada.coluna] = PRETA;
     return 1;
