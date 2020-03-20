10be715114884f2bfcbf10baae559d0f8fc68ccc
Nelson Ribeiro <62015213+NelsonR19@users.noreply.github.com>
2020-03-20 15:52:31 +0000

    Update logica.h

--- logica.h
+++ logica.h
@@ -1,6 +1,26 @@
+/**
+@file logica.h
+Lógica do progama
+*/
+
 #ifndef ___LOGICA_H___
 #define ___LOGICA_H___
 
+/**
+\brief Função que verifica se uma jogada é válida
+@param e Apontador para o estado
+@param c_jogada A coordenada da jogada
+@returns Verdadeiro (valor diferente de zero) se for válida e Falso (zero) se não for
+*/
+int jogada_valida(ESTADO *e, COORDENADA c_jogada);
+
+/**
+\brief Função que modifica o estado ao jogar na casa correta se a jogada for válida
+@param e Apontador para o estado
+@param c A coordenada
+@returns Verdadeiro (valor diferente de zero) se for possível jogar e Falso (zero) caso não seja possível
+*/
 int jogar(ESTADO *e, COORDENADA c);
+
 
 #endif
