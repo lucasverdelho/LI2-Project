8011c192cefed9ce217e03565d30a790464831f8
Nelson Ribeiro <62015213+NelsonR19@users.noreply.github.com>
2020-03-20 15:24:00 +0000

    Update interface.h

--- interface.h
+++ interface.h
@@ -1,7 +1,24 @@
+/**
+@file interface.h
+Interpretador de comandos e mostra Tabuleiro
+*/
+
 #ifndef ___INTERFACE_H___
 #define ___INTERFACE_H___
 
+/**
+\brief Função que devolve o tabuleiro
+*/
 void mostrar_tabuleiro(ESTADO *e);
+
+/**
+\brief Função que devolve o erro correspondente
+*/
+void print_erro(ERROS e);
+
+/**
+\brief Função que interpreta os comandos
+*/
 int interpretador(ESTADO *e);
 
 #endif
