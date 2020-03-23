c893c58f754fea2f0be044014af8628b653e6c54
João Tiago Alves Fidalgo de Sousa <62015233+tsousa111@users.noreply.github.com>
2020-03-23 15:57:33 +0000

    Update main.c

--- main.c
+++ main.c
@@ -2,10 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 #include "dados.h"
+#include "interface.h"
 
 // Função que deve ser colocada no ficheiro main.c
 int main(){
     ESTADO *e = inicializar_estado();
-    interpretador(e);
+    mostrar_tabuleiro(e);
+    while (interpretador(e));
     return 0;
 }
