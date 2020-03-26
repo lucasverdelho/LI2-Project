9caca9e664730da1a6bd4b6fa0d6c1775d0542f7
André Lucas SIlva Verdelho <62015174+LucasVerdelho@users.noreply.github.com>
2020-03-26 16:17:53 +0000

    Update dados.h

--- dados.h
+++ dados.h
@@ -94,4 +94,8 @@ int obter_numero_de_jogadas(ESTADO *estado);
 */
 CASA obter_estado_casa(ESTADO *e, COORDENADA c);
 
+void mete_casa_branca(ESTADO *e,COORDENADA c);
+
+void mete_casa_preta(ESTADO *e);
+
 #endif
