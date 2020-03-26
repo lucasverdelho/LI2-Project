39433fe9e4161a8b481f8c6804d8cba80ba8261c
André Lucas SIlva Verdelho <62015174+LucasVerdelho@users.noreply.github.com>
2020-03-26 17:50:28 +0000

    Update logica.c

--- logica.c
+++ logica.c
@@ -11,50 +11,50 @@
 //     return 0;
 // }
 
-ERROS casas_livres(ESTADO *e) {return OK;}
-// {
-//     COORDENADA c_atual = {e->ultima_jogada.linha,e->ultima_jogada.coluna};
-//     for (int linhas = c_atual.linha - 1 ; linhas <= c_atual.linha + 1; linhas++)
-//     {
-//         if (linhas == -1);
-//         else if (linhas == 8)
-//             break;
-//         else 
-//         {
-//             for (int colunas = c_atual.coluna-1; colunas <= c_atual.coluna + 1; colunas++)
-//             {
-//                 if (colunas == -1);
-//                 else if (colunas == 8)
-//                     break;
-//                 else 
-//                 {
-//                     COORDENADA validar = {linhas,colunas};
-//                     if (obter_estado_casa(e,validar) == PRETA);
-//                     else if (obter_estado_casa(e,validar) == BRANCA)
-//                         return OK;
-//                 }
-//             }
-//         }
-//     }
-//     return JOGADA_INVALIDA;
-// }
+ERROS casas_livres(ESTADO *e)
+{
+    COORDENADA c_atual = {e->ultima_jogada.linha,e->ultima_jogada.coluna};
+    for (int linhas = c_atual.linha - 1 ; linhas <= c_atual.linha + 1; linhas++)
+    {
+        if (linhas == -1);
+        else if (linhas == 8)
+            break;
+        else 
+        {
+            for (int colunas = c_atual.coluna-1; colunas <= c_atual.coluna + 1; colunas++)
+            {
+                if (colunas == -1);
+                else if (colunas == 8)
+                    break;
+                else 
+                {
+                    COORDENADA validar = {linhas,colunas};
+                    if (obter_estado_casa(e,validar) == PRETA);
+                    else if (obter_estado_casa(e,validar) == BRANCA)
+                        return OK;
+                }
+            }
+        }
+    }
+    return JOGADA_INVALIDA;
+}
 
-int jogar(ESTADO *e, COORDENADA c)
+int jogar(ESTADO *e, COORDENADA c,int *vence_j1, int *vence_j2)
 {
     if (casas_livres(e) == OK)
     {
         if ((obter_estado_casa(e,c)!= PRETA) && (obter_estado_casa(e,c)!= BRANCA))
         {
-            int linha = c.linha;
+            int linha = 7-c.linha;
             int coluna = c.coluna;
             //atualizar_jogada(e,c,obter_jogador_atual (e));
             mete_casa_branca(e,c);
             mete_casa_preta(e);
-            if (linha == 0 && coluna == 7) {} // ganha jogador 2
-            else if (linha == 7 && coluna == 0) {} // ganha jogador 1
+            if (linha == 0 && coluna == 7) *vence_j2 = 1; // ganha jogador 2
+            else if (linha == 7 && coluna == 0) *vence_j1 = 1; // ganha jogador 1
         }
     }
-    else {}; // ganha o jogador que bloqueou o outro
+    else *vence_j1 = 1; // ganha o jogador que bloqueou o outro
     return 1;
 }
 
