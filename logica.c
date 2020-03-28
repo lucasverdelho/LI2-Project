26e43c1e0df37c8b64d0b474d1bf25a71be90949
João Tiago Alves Fidalgo de Sousa <62015233+tsousa111@users.noreply.github.com>
2020-03-28 15:15:17 +0000

    Update logica.c

--- logica.c
+++ logica.c
@@ -47,14 +47,22 @@ int jogar(ESTADO *e, COORDENADA c,int *vence_j1, int *vence_j2)
         {
             int linha = 7-c.linha;
             int coluna = c.coluna;
-            //atualizar_jogada(e,c,obter_jogador_atual (e));
+            atualizar_jogada(e,c);
+//            if(obter_jogador_atual(e) == 1){
+//                altera_jogador(e,obter_jogador_atual(e));
+//            }
+//            else {
+//                altera_jogador(e,obter_jogador_atual(e));
+//                add_num_jogadas(e);
+//            }
+            mudar_ultima_jogada(e,c);
             mete_casa_branca(e,c);
             mete_casa_preta(e);
-            if (linha == 0 && coluna == 7) vence_j2 = 1; // ganha jogador 2
-            else if (linha == 7 && coluna == 0) vence_j1 = 1; // ganha jogador 1
+            if (linha == 0 && coluna == 7) *vence_j2 = 1; // ganha jogador 2
+            else if (linha == 7 && coluna == 0) *vence_j1 = 1; // ganha jogador 1
         }
     }
-    else vence_j1 = 1; // ganha o jogador que bloqueou o outro
+    else *vence_j1 = 1; // ganha o jogador que bloqueou o outro
     return 1;
 }
 
