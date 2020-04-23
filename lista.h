c4b3c6c1ce1903811937564b9ee86bb99f5f79a2
João Tiago Alves Fidalgo de Sousa <62015233+tsousa111@users.noreply.github.com>
2020-04-23 18:19:42 +0100

    Create lista.h

---
+++ lista.h
@@ -0,0 +1,22 @@
+#ifndef ___LOGICA_H___
+#define ___LOGICA_H___
+
+typedef struct nodo {
+   void *valor;
+   struct nodo *proximo;
+} NODO, *LISTA;
+
+// Cria uma lista vazia
+LISTA criar_lista();
+// Insere um valor na cabeça da lista
+LISTA insere_cabeca(LISTA L, void *valor);
+// Devolve a cabeça da lista
+void *devolve_cabeca(LISTA L);
+// Devolve a cauda da lista
+LISTA proximo(LISTA L);
+// Remove a cabeça da lista (libertando o espaço ocupado) e devolve a cauda
+LISTA remove_cabeca(LISTA L);
+// Devolve verdareiro se a lista é vazia
+int lista_esta_vazia(LISTA L);
+
+#endif
