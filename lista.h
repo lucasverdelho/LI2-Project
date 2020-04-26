61bf41758a34a0491db449acf9368b24b2e15045
João Tiago Alves Fidalgo de Sousa <62015233+tsousa111@users.noreply.github.com>
2020-04-26 16:31:59 +0100

    Update lista.h

--- lista.h
+++ lista.h
@@ -1,22 +1,58 @@
+/**
+@file lista.h
+Módulo de listas ligadas
+*/
 #ifndef ___LISTA_H___
 #define ___LISTA_H___
 
+/**
+\brief Tipo de dados para Listas
+*/
 typedef struct nodo {
    void *valor;
    struct nodo *proximo;
 } NODO, *LISTA;
 
-// Cria uma lista vazia
+/**
+\brief Cria uma lista vazia
+@returns A lista
+*/
 LISTA criar_lista();
-// Insere um valor na cabeça da lista
+
+/**
+\brief Insere um valor na cabeça da lista
+@param L Lista
+@param valor Apontador para a cabeça da lista
+@returns A lista
+*/
 LISTA insere_cabeca(LISTA L, void *valor);
-// Devolve a cabeça da lista
+
+/**
+\brief Devolve a cabeça da lista
+@param L Lista
+@returns A cabeça da lista
+*/
 void *devolve_cabeca(LISTA L);
-// Devolve a cauda da lista
+
+/**
+\brief Devolve a cauda da lista
+@param L Lista
+@returns A cauda da lista
+*/
 LISTA proximo(LISTA L);
-// Remove a cabeça da lista (libertando o espaço ocupado) e devolve a cauda
+
+/**
+\brief Remove a cabeça da lista (libertando o espaço ocupado) e devolve a cauda
+@param L Lista
+@returns A cauda da lista
+*/
 LISTA remove_cabeca(LISTA L);
-// Devolve verdareiro se a lista é vazia
+
+/**
+\brief Devolve verdareiro se a lista é vazia
+@param L Lista
+@returns Verdadeiro (valor diferente de zero) se for estiver vazia e Falso (zero) caso contrário
+*/
 int lista_esta_vazia(LISTA L);
 
 #endif
